db563dbb22439bca0dca3deb48e8d2554fec6975
DeMariaMattia <mattiademaria03@gmail.com>
2022-04-06 22:59:24 +0200

    Modifiche

--- Progetto_MYC/Operazioni.js
+++ Progetto_MYC/Operazioni.js
@@ -1,4 +1,8 @@
 function TrovaStringa(){
  var Stringa= document.getElementById("TxtStringa").value;
  alert("il tuo codice è: "+Stringa);
+<<<<<<< Updated upstream
+=======
+ document.getElementById("TxtStringa").value="";
+>>>>>>> Stashed changes
 }
